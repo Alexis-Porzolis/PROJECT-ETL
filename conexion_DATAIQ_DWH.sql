9f79a14c037cb7b995f6520c38ed4192bc1ced49
Alexis <38305415+Alexis-Porzolis@users.noreply.github.com>
2024-09-09 00:41:00 -0300

    add query-conexiones-local

---
+++ conexion_DATAIQ_DWH.sql
@@ -0,0 +1,3 @@
+--En este caso me estoy conectando de manera local 
+ATTACH DATABASE 'C:\Users\ALEXIS\Desktop\dataIQ\DATAIQ_DWH.db' AS dwh;
+ATTACH DATABASE 'C:\Users\ALEXIS\Desktop\dataIQ\DATAIQ_STG.db' AS stg;
